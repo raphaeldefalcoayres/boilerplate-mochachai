c07b1ed63487b7317dac54fe9f551bf2f7553911
Raphael de Falco Ayres <raphaeldefalcoayres@gmail.com>
2019-12-10 14:37:07 -0300

    test fix

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -69,6 +69,22 @@ suite("Functional Tests", function() {
             done(); // Always call the 'done()' callback when finished.
           });
       });
+      test("Test GET /hello with no name", function(done) {
+        // Don't forget the callback...
+        chai
+          .request(server) // 'server' is the Express App
+          .get("/hello") // http_method(url). NO NAME in the query !
+          .end(function(err, res) {
+            // res is the response object
+
+            // Test the status and the text response (see the example above).
+            // Please follow the order -status, -text. We rely on that in our tests.
+            // It should respond 'Hello Guest'
+            assert.equal(res.status, 200);
+            assert.equal(res.text, "hello Guest");
+            done(); // Always call the 'done()' callback when finished.
+          });
+      });
 
       /**  Another one... **/
       test("Test GET /hello with your name", function(done) {
