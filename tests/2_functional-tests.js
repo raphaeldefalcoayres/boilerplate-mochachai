976e5d93ce87d8809900a59baaa2645349a87ec6
Raphael de Falco Ayres <raphaeldefalcoayres@gmail.com>
2019-12-10 18:55:51 -0300

    Fix Tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -148,25 +148,14 @@ suite("Functional Tests", function() {
         chai
           .request(server)
           .put("/travellers")
+          /** send {surname: 'Colombo'} here **/
           .send({ surname: "Colombo" })
           .end(function(err, res) {
             /** your tests here **/
-            assert.equal(res.status, 200, "response status should be 200");
-            assert.equal(
-              res.type,
-              "application/json",
-              "Response should be json"
-            );
-            assert.equal(
-              res.body.name,
-              "Cristoforo",
-              'res.body.name should be "Christoforo"'
-            );
-            assert.equal(
-              res.body.surname,
-              "Colombo",
-              'res.body.surname should be "Colombo"'
-            );
+            assert.equal(res.status, 200);
+            assert.equal(res.type, "application/json");
+            assert.equal(res.body.name, "Cristoforo");
+            assert.equal(res.body.surname, "Colombo");
 
             done(); // Never forget the 'done()' callback...
           });
@@ -176,10 +165,18 @@ suite("Functional Tests", function() {
       // Try it again. This time without help !!
       test('send {surname: "da Verrazzano"}', function(done) {
         /** place the chai-http request code here... **/
-
+        chai
+          .request(server)
+          .put("/travellers")
+          .send({ surname: "da Verrazzano" })
+          .end((err, res) => {
+            assert.equal(res.status, 200);
+            assert.equal(res.type, "application/json");
+            assert.equal(res.body.name, "Giovanni");
+            assert.equal(res.body.surname, "da Verrazzano");
+          });
         /** place your tests inside the callback **/
 
-        assert.fail(); // remove this after adding tests
         done();
       });
     });
